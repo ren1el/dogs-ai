2dcbb4712e7f8a2cca5e6816cef06ff1fc24bd13
Reniel Ocampo <renielxocampo@gmail.com>
2021-01-15 15:20:19 -0800

    Add infinite scrolling

--- src/components/DogsView.js
+++ src/components/DogsView.js
@@ -1,18 +1,32 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
+import { addDogs } from '../reducers/dogsReducer'
+import InfiniteScroll from 'react-infinite-scroll-component'
 import Dog from './Dog'
 
 const DogsView = () => {
+  const dispatch = useDispatch()
   const dogs = useSelector(state => state.dogs)
+  const filter = useSelector(state => state.filter)
+
+  const fetchMoreDogs = () => {
+    dispatch(addDogs(filter))
+  }
 
   return (
     <div>
       <h2>DogsView Component</h2>
 
       <div>
-        {dogs.map(dog => (
-          <Dog key={dog.url} url={dog.url} breed={dog.breed} />
-        ))}
+        <InfiniteScroll
+          dataLength={dogs.length}
+          next={fetchMoreDogs}
+          hasMore={true}
+        >
+          {dogs.map(dog => (
+            <Dog key={dog.url} url={dog.url} breed={dog.breed} />
+          ))}
+        </InfiniteScroll>
       </div>
     </div>
   )
