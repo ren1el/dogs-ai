8da7fca0065979c8f80bbeea308bb25254944bdb
Reniel Ocampo <renielxocampo@gmail.com>
2021-01-15 16:38:57 -0800

    Add styled components and organize images into grid

--- src/components/Dog.js
+++ src/components/Dog.js
@@ -1,10 +1,20 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const StyledDog = styled.div`
+  img {
+    width: 100%;
+    object-fit: contain;
+    display: block;
+    margin-bottom: 1rem;
+  }
+`
 
 const Dogs = ({ url, breed }) => {
   return (
-    <p>
-      breed: {breed} url: <img src={url} alt={breed} />
-    </p>
+    <StyledDog>
+      <img src={url} alt={breed} />
+    </StyledDog>
   )
 }
 
--- src/components/DogsView.js
+++ src/components/DogsView.js
@@ -2,7 +2,16 @@ import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { addDogs } from '../reducers/dogsReducer'
 import InfiniteScroll from 'react-infinite-scroll-component'
+import styled from 'styled-components'
 import Dog from './Dog'
+
+const StyledGrid = styled.div`
+  width: 1280px;
+  display: grid;
+  gap: 1rem;
+  grid-template-columns: repeat(3, minmax(0, 1fr));
+  grid-template-rows: 1fr;
+`
 
 const DogsView = () => {
   const dispatch = useDispatch()
@@ -13,21 +22,50 @@ const DogsView = () => {
     dispatch(addDogs(filter))
   }
 
+  let counter = 0
+  const dog_groups = [[], [], []]
+
+  dogs.forEach(dog => {
+    dog_groups[counter].push(
+      <Dog key={dog.url} url={dog.url} breed={dog.breed} />
+    )
+
+    if (counter >= 2) {
+      counter = 0
+    } else {
+      counter++
+    }
+  })
+
+  console.log(dog_groups)
+
   return (
     <div>
       <h2>DogsView Component</h2>
 
-      <div>
+      <StyledGrid>
         <InfiniteScroll
           dataLength={dogs.length}
           next={fetchMoreDogs}
           hasMore={true}
         >
-          {dogs.map(dog => (
-            <Dog key={dog.url} url={dog.url} breed={dog.breed} />
-          ))}
+          <div>{dog_groups[0]}</div>
         </InfiniteScroll>
-      </div>
+        <InfiniteScroll
+          dataLength={dogs.length}
+          next={fetchMoreDogs}
+          hasMore={true}
+        >
+          <div>{dog_groups[1]}</div>
+        </InfiniteScroll>
+        <InfiniteScroll
+          dataLength={dogs.length}
+          next={fetchMoreDogs}
+          hasMore={true}
+        >
+          <div>{dog_groups[2]}</div>
+        </InfiniteScroll>
+      </StyledGrid>
     </div>
   )
 }
